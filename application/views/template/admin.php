0a3132decf74360e9638628a6cd307d5d2c5ea0d
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-03-19 05:13:55 +0700

    wes
    wes mari

---
+++ application/views/template/admin.php
@@ -0,0 +1,16 @@
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+
+            <div class="row mt-5">
+                <h1>Welcome To Koridor</h1>
+            </div>
+
+
+
+        </div>
+    </main>
