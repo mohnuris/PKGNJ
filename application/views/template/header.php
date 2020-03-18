f406014f204018506974cbbdb7976e9700179aef
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-03-19 05:27:40 +0700

    dasboard
    menambahkan floder

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,7 +9,7 @@
   <meta name="description" content="" />
   <meta name="author" content="" />
   <title>Static Navigation - SB Admin</title>
-  <link href="<?= base_url('assets/') ?>css/styles.css" rel="stylesheet" />
+  <link href="<?= base_url('template/css/styles.css') ?>" rel="stylesheet" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous">
   </script>
 </head>
