54fe7db896111fa070316d2d077208dc2ae4771a
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-04-03 23:08:37 +0700

    relasi

---
+++ application/views/charts/coba.php
@@ -0,0 +1,12 @@
+<div id="layoutSidenav_content">
+  <main>
+    <div class="container-fluid">
+      <h1 class="mt-4">Tables</h1>
+      <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item"><a href=" <?= base_url('admin') ?>">Dashboard</a></li>
+        <li class="breadcrumb-item"><a href=" <?= base_url('admin/c_triwulan1') ?>">Tables</a></li>
+
+      </ol>
+      <div id="chart_div" style="width: 900px; height: 500px;"></div>
+
+     
