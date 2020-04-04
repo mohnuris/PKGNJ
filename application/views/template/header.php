386e5caf96f4a6c828c7af05e80c938d00d31ad8
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-04-04 17:49:50 +0700

    Update header.php

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -12,7 +12,7 @@
   <!-- logo -->
   <link rel="shortcut icon" href="<?= base_url('template/icons/logo.png') ?>">
 
-  
+
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -33,7 +33,7 @@
 
 <body class="sb-nav-fixed">
   <nav class="sb-topnav navbar navbar-expand navbar-info bg-primary">
-    <a class="navbar-brand text-white text-bol" href="<?= base_url('admin') ?>">BirPendNJ</a>
+    <a class="navbar-brand text-white text-boll" href="<?= base_url('admin') ?>">BirPendNJ</a>
     <button class="btn btn-primary btn-sm order-1 bg-link order-lg-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars">
       </i></button>
@@ -49,19 +49,49 @@
     <!-- Navbar-->
     <ul class="navbar-nav ml-auto ml-md-0">
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-white" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+      <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+          <img src="<?= base_url('icon/pria.png') ?>" alt="" width="30px">
+          <div class="d-sm-none d-lg-inline-block text-white">Hi, Ujang Maman</div>
+        </a>
 
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="#">
-            <i class="ace-icon fa fa-user"></i> Profil</a>
 
-          <a class="dropdown-item" href="#">
-            <i class="ace-icon fa fa-cog"></i> Settings</a>
-
-          <a class="dropdown-item" href="login.html">
-            <i class="fas fa-sign-out-alt"></i> Logout</a>
-          <!-- <i class="ace-icon fa fa-power-off"></i> -->
+        <div class="dropdown-menu dropdown-menu-right shadow btn-white">
+          <!-- <div class="card mb-5  bg-white rounded"> -->
+          <div class="dropdown-title"></div>
+          <a href="features-profile.html" class="dropdown-item has-icon">
+            <i class="far fa-user"></i> Profile
+          </a>
+          <a href="features-activities.html" class="dropdown-item has-icon">
+            <i class="fas fa-bolt"></i> Activities
+          </a>
+          <a href="features-settings.html" class="dropdown-item has-icon">
+            <i class="fas fa-cog"></i> Settings
+          </a>
+          <!-- <div class="dropdown-divider"></div> -->
+          <a href="#" class="dropdown-item has-icon text-danger">
+            <i class="fas fa-sign-out-alt"></i> Logout
+          </a>
+        </div>
         </div>
       </li>
+      </li>
+    </ul>
+    <!-- 
+      <ul class="navbar-nav ml-auto ml-md-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="#">
+              <i class="ace-icon fa fa-user"></i> Profil</a>
+
+            <a class="dropdown-item" href="#">
+              <i class="ace-icon fa fa-cog"></i> Settings</a>
+
+            <a class="dropdown-item" href="login.html">
+              <i class="fas fa-sign-out-alt"></i> Logout</a>
+            <!-- <i class="ace-icon fa fa-power-off"></i> -->
+    </div>
+    </li>
     </ul>
   </nav>
