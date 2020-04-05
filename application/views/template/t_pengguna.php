df799a895136773abd59ad14d1a98e80df530f95
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-04-06 00:55:04 +0700

    Update t_pengguna.php

--- application/views/template/t_pengguna.php
+++ application/views/template/t_pengguna.php
@@ -16,7 +16,7 @@
                     <div class="table-responsive">
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                             <thead>
-                                <tr class="bg-primary text-light">
+                                <tr class="text-black">
                                     <th>Name</th>
                                     <th>Position</th>
                                     <th>Office</th>
