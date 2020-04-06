1fed84fd93cba4b1d636bd1dfb443358936f20ff
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-04-06 17:33:49 +0700

    Update Admin_model.php

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9,23 +9,28 @@ class Admin_model extends CI_Model
       ->order_by($urut_id, 'DESC')
       ->get('');
   }
+
   public function simpandata($table, $data)
   {
     return $this->db->insert($table, $data);
   }
+
   public function hapusdata($table, $id, $primary)
   {
     return $this->db->delete($table, array($primary => $id));
   }
+
   public function formedit($table, $primary, $id)
   {
     return $this->db->get_where($table, array($primary => $id))->row();
   }
+
   public function editdata($table, $primary, $id, $data)
   {
     return $this->db->where($primary, $id)
       ->update($table, $data);
   }
+
   public function joinsiswa()
   {
     $query = $this->db->select('*')
@@ -36,6 +41,7 @@ class Admin_model extends CI_Model
 
     return $query;
   }
+
   public function comboxdinamis()
   {
     $query = $this->db->get('tb_guru');
