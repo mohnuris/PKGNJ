58dbe2aab2decfd1f85fc3a44bb0b59d38d6079d
MohNuris <nuris.ft.unuja@gmail.com>
2020-04-16 00:43:11 +0700

    menambahkan validasi pada form input dan edit

--- application/views/formulir/f_kelas.php
+++ application/views/formulir/f_kelas.php
@@ -7,22 +7,6 @@
         <li class="breadcrumb-item active">Form Tambah Kelas</li>
       </ol>
 
-
-      <!-- <alert Errors"> -->
-      <!-- <?php
-            if (validation_errors()) {
-            ?>
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-          <strong>
-            Maaf Data Gagal Disimpan
-            <?php echo validation_errors(); ?>
-          </strong>
-          <button type="button" class="close" data-dismiss="alert" aria-label="close"> <span class="fa fa-times"></span></button>
-        </div>
-      <?php
-
-            }
-      ?> -->
       <div class="card mb-5 shadow p-3 mb-5 bg-white rounded">
         <!-- <div class="modal-header bg-primary text-white">
 
@@ -35,10 +19,14 @@
           <!-- form input -->
           <label for="kl" class="alert-link">Kelas</label>
           <?php echo form_input("kl", "", array('class' => 'form-control', 'id' => 'kl', 'placeholder' => 'Kelas dan Jurusan')); ?>
+          <small class="text-danger">
+            <?php echo form_error('kl', ' '); ?>
+          </small>
+
 
 
           <div class="modal-footer">
-          <button type="submit" id="simpan" onclick="Swal.fire('SUKSES','Data Tersimpan','success')" class="btn btn-primary">SIMPAN</button>
+            <button type="submit" id="simpan" onclick="Swal.fire('SUKSES','Data Tersimpan','success')" class="btn btn-primary">SIMPAN</button>
             <!-- <?php echo form_submit('save', 'SIMPAN', array('class' => 'btn btn-primary  mt-4 pr-4 pl-4')); ?> -->
             <?php echo form_close(); ?>
 
