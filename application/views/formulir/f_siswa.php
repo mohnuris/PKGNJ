37f3198c1f4d65d8a6d63c706b20458223af075d
MohNuris <nuris.ft.unuja@gmail.com>
2020-04-28 04:15:46 +0700

    Formulir Data Siswa  dan Validasinya

--- application/views/formulir/f_siswa.php
+++ application/views/formulir/f_siswa.php
@@ -1,77 +1,49 @@
 <div id="layoutSidenav_content">
   <main>
     <div class="container-fluid">
-      <h3 class="mt-5">Form Siswa</h3>
+      <h3 class="mt-5"></h3>
       <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item"><a href=" <?= base_url('admin/t_siswa') ?>">Siswa</a></li>
         <li class="breadcrumb-item active">Form Siswa</li>
       </ol>
 
+      <h3 class="mt-5">Form Siswa</h3>
 
       <!-- <div class="modal"> -->
       <div class="card mb-5 shadow p-3 mb-5 bg-white rounded">
-        <!-- <div class="modal-header bg-primary text-white">
-
-          <h3 class="modal-title" id="exampleModalLabel"></h3>
-        </div> -->
 
         <div class="modal-body">
           <?php echo form_open('admin/sm_siswa'); ?>
-          <div class="form-group">
-            <label for="s" class="alert-link">Nama Siswa</label>
-
-            <?php echo form_input("nm_siswa", set_value('nm_siswa'), array('class' => 'form-control', 'id' => 's', 'placeholder' => 'Nama Lengkap')); ?>
-            <small class="text-danger">
-              <?php echo form_error('nm_siswa', ' '); ?>
-            </small>
-
-            <br>
-            <label form="jk" class="alert-link">Jenis Kelamin</label><br>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" checked>
-              <label class="form-check-label" for="exampleRadios1">
-                Laki-Laki
-                </br>
-
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" <label class="form-check-label" for="exampleRadios2">
-                Perempuan
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <!-- <div class="modal-body">
-          <label for="" class="alert-link">Pilih Kelas</label>
-          <label class="mr-sm-2 sr-only" for="inlineFormCAustomSelect">Preference</label>
-          <select class="custom-select" id="inlineFormCustomSelect">
-            <option selected>Pilih</option>
-            <option value="1">X </option>
-            <option value="3">XI</option>
-            <option value="3">XII</option>
-          </select>
-        </div> -->
-
-        <div class="modal-body">
-          <label for="" class="alert-link">Kelas dan Jurusan</label>
-          <label class="mr-sm-2 sr-only form-control-lg" for="inlineFormCustomSelect"></label>
-          <select class="custom-select" id="inlineFormCustomSelect">
-            <option selected>------Pilih-----</option>
-            <option value="1">MULTIMEDIA 1</option>
-            <option value="1">MULTIMEDIA 2</option>
-            <option value="2">Rekayasa Perangkat Lunak (RPL) 1</option>
-            <option value="3">Teknik Komputer Jaringan (TKJ) 1</option>
-            <option value="3"> Teknik Pembangkit Tenaga Listrik (TPTL)</option>
-            <option value="3"> Agribisnis Pengolahan Hasil Perikanan (APHP)</option>
-            <option value="3">Tata Busana (TB)</option>
-
-          </select>
+          <!-- <div class="form-group"> -->
+          <label for="nm" class="alert-link">Nama Siswa</label>
+          <?php echo form_input("nm_siswa", set_value('nm_siswa'), array('class' => 'form-control', 'id' => 'nm', 'placeholder' => 'Nama Lengkap')); ?>
+          <small class="text-danger">
+            <?php echo form_error('nm', ' '); ?>
+          </small>
         </div>
 
 
         <div class="modal-body">
-          <label for="" class="alert-link">Pilih Guru</label>
-          <label class="mr-sm-2 sr-only form-control" for="inlineFormCustomSelect1"></label>
-          <select class="custom-select" id="inlineFormCustomSelect1">
+          <label for="sk" class="alert-link">Sekolah / lembaga</label>
+          <?php echo form_input("sk", "", array('class' => 'form-control', 'id' => 'sk', 'placeholder' => 'Sekolah / lembaga')); ?>
+          <small class="text-danger">
+            <?php echo form_error('sk', ' '); ?>
+          </small>
+        </div>
+
+        <div class="modal-body">
+          <label for="kl" class="alert-link">Kelas dan Jurusan</label>
+          <label class="mr-sm-2 sr-only form-control-lg" for="kl"></label>
+          <?php echo form_input("kl", "", array('class' => 'form-control', 'id' => 'kl', 'placeholder' => 'Kelas dan Jurusan')); ?>
+          <small class="text-danger">
+            <?php echo form_error('kl', ' '); ?>
+          </small>
+        </div>
+
+        <div class="modal-body">
+          <label for="gr" class="alert-link">Pilih Guru</label>
+          <label class="mr-sm-2 sr-only form-control" for="gr"></label>
+          <select class="custom-select" id="gr">
             <option selected>------Pilih-----</option>
             <option value="1">Mujiburrohman</option>
             <option value="2">Maziyyatus Sholihah</option>
@@ -107,29 +79,44 @@
             <option value="3">Hairul Umam</option>
 
           </select><br>
+        </div>
+
+        <div class="modal-body">
+          <label for="jk" class="alert-link">Jenis Kelamin</label><br>
+          <?php echo form_radio('jk_siswa', 'laki-laki', set_value('jk')) ?>Laki-Laki
+          <?php echo form_radio('jk_siswa', 'perempuan', set_value('jk')) ?>Perempuan
           <br>
-          <form>
-            <div class="form-group">
-              <label for="exampleFormControlFile1" class="alert-link">Foto Siswa</label>
-              <input type="file" class="form-control-file" id="exampleFormControlFile1">
-            </div>
-          </form>
+          <small class="text-danger">
+            <?php echo form_error('jk', ' '); ?>
+          </small>
+        </div>
 
-          <div class="modal-footer">
-            <button type="submit" id="simpan" onclick="Swal.fire('SUKSES','Data Tersimpan','success')" class="btn btn-primary">SIMPAN</button>
-            <?php echo form_close(); ?>
+        <div class="modal-body">
+          <label for="foto" class="alert-link">Foto Siswa</label><br>
+          <?php echo form_upload('foto', '', array('class' => 'footo')); ?>
+        </div>
 
-            <script>
-              const tombol = document.querySelector('#edit');
-              tombol.addEventListener('click', function() {
-                Swal({
-                  title: 'SUKSES',
-                  text: 'Edit Data Tahun',
-                  type: 'success'
-                })
-              });
-            </script>
+        <!-- <form>
+          <div class="form-group">
+            <label for="exampleFormControlFile1" class="alert-link">Foto Siswa</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1">
           </div>
+        </form> -->
+
+        <div class="modal-footer">
+          <button type="submit" id="simpan" onclick="Swal.fire('SUKSES','Data Tersimpan','success')" class="btn btn-primary">SIMPAN</button>
+          <?php echo form_close(); ?>
+
+          <script>
+            const tombol = document.querySelector('#edit');
+            tombol.addEventListener('click', function() {
+              Swal({
+                title: 'SUKSES',
+                text: 'Edit Data Tahun',
+                type: 'success'
+              })
+            });
+          </script>
         </div>
       </div>
     </div>
