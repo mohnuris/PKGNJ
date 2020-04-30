c5b8ef8aa9858f23f048079a6274eb5e2485a565
MohNuris <nuris.ft.unuja@gmail.com>
2020-05-01 02:59:56 +0700

    menambahkan id_guru dan ft_guru
    pada kolom eguru

--- application/views/formulir/E_guru.php
+++ application/views/formulir/E_guru.php
@@ -9,45 +9,67 @@
 
       <div class="card mb-5 shadow p-3 mb-5 bg-white rounded">
         <div class="modal-body align-center">
-          <?php echo form_open('Admin/ed_guru'); ?>
-          <!-- <?php echo form_hidden('id', $gr->id_guru); ?> -->
-          <?php echo form_hidden('id', $gr->id_guru); ?>
-          <?php echo form_hidden('id', $gr->ft_guru); ?>
+          <?php echo form_open('admin/edit_guru'); ?>
+          <?php echo form_hidden("id_guru", $gr->id_guru); ?>
+          <?php echo form_hidden("foto", $gr->ft_guru); ?>
 
           <form action="<?= base_url('admin/guru/') ?>{id_guru}">
-            <label for="niup" class="alert-link">Niup</label>
 
+            <label for="niup" class="alert-link">Niup</label>
             <?php echo form_input("niup", $gr->niup, array('class' => 'form-control', 'id' => 'niup', 'placeholder' => 'NIUP ')); ?>
+            <small class="text-danger">
+              <?php echo form_error('niup', ' '); ?>
+            </small>
             <br>
+
             <form>
               <label for="nm_guru" class="alert-link">Nama Lengkap </label>
               <?php echo form_input("nm_guru", $gr->nm_guru, array('class' => 'form-control', 'id' => 'nm_guru', 'placeholder' => 'Nama Lengkap')); ?>
+              <small class="text-danger">
+                <?php echo form_error('nm_guru', ' '); ?>
+              </small>
 
               <br>
               <label for="sekolah" class="alert-link">Sekolah </label>
               <?php echo form_input("sekolah", $gr->sekolah, array('class' => 'form-control', 'id' => 'sekolah', 'placeholder' => ' Sekolah')); ?>
+              <small class="text-danger">
+                <?php echo form_error('sekolah', ' '); ?>
+              </small>
 
               <br>
               <label for="kelas" class="alert-link">Kelas </label>
               <?php echo form_input("kelas", $gr->kelas, array('class' => 'form-control', 'id' => 'kelas', 'placeholder' => ' Kelas')); ?>
+              <small class="text-danger">
+                <?php echo form_error('kelas', ' '); ?>
+              </small>
 
               <br>
               <label for="jurusan" class="alert-link">Jurusan </label>
               <?php echo form_input("jurusan", $gr->jurusan, array('class' => 'form-control', 'id' => 'jurusan', 'placeholder' => ' jurusan')); ?>
+              <small class="text-danger">
+                <?php echo form_error('jurusan', ' '); ?>
+              </small>
+              <br>
+
+              <label for="mapel" class="alert-link">Mata Pelajaran </label>
+              <?php echo form_input("mapel", $gr->mapel, array('class' => 'form-control', 'id' => 'mapel', 'placeholder' => ' Mata Pelajaran')); ?>
+              <small class="text-danger">
+                <?php echo form_error('mapel', ' '); ?>
+              </small>
 
               <br>
               <div class="from-grup">
-                <label for="jk_guru" class="alert-link">Jenis Kelamin</label><br>
+                <label for="jk_guru" class="alert-link">Jenis Kelamin</label>
                 <?php
                 $jk_guru = $gr->jk_guru;
                 if ($jk_guru == "pria") {
-                ?><br><?php echo form_radio('jk_guru', 'laki-laki', 'checked') ?> Laki-Laki
+                ?><br><?php echo form_radio("jk_guru", 'laki-laki', 'checked') ?> Laki-Laki
                   <?php echo form_radio('jk_guru', 'perempuan', '') ?> perempuan
-                  <br>
+
                 <?php
                 } else {
-                ?><br><?php echo form_radio('jk_guru', 'laki-laki', 'checked') ?> Laki-Laki
-                  <?php echo form_radio('jk_guru', 'perempuan', '') ?> perempuan
+                ?><br><?php echo form_radio('jk_guru', 'laki-laki', '') ?> Laki-Laki
+                  <?php echo form_radio('jk_guru', 'perempuan', 'checked') ?> perempuan
                 <?php
                 }
 
@@ -59,30 +81,45 @@
               </div>
               <br>
               <div class="from-grup">
-                <label for="foto" class="alert-link">Foto Guru*)</label>
-                <?php
-                if (!$gr->ft_guru) {
-                ?>
-                  <img src="<?= base_url('assets/guru/avatar.gif') ?>" alt="" width="100px">
-                  <br>
-                <?php
-                } else {
-                ?>
-                  <img src="<?= base_url('assets/guru/' . $gr->ft_guru) ?>" alt="" width="300px">
-                <?php
-                }
-
-                ?>
-                <?php echo form_upload('foto', '', $gr->ft_guru, array('class' => 'foto')); ?>
-
+                <label for="foto" class="alert-link">Foto Guru*)</label><br>
+                <?php echo form_upload('foto', '', $gr->ft_guru, array('class' => 'foto')); ?><br>
                 <small class="text-danger">
-                  <?php echo form_error(('ft_guru')); ?>
+                  <?php echo $errors; ?>
+                  <!-- <?php echo form_error(('ft_guru')); ?> -->
                 </small>
-              </div>
+                <div><br>
+                  <?php
+                  if (!$gr->ft_guru) {
+                    if ($gr->jk_guru == 'laki-laki') {
+                  ?>
+                      <img src="<?= base_url('icon/pria.png') ?>" alt="" width="40px">
+                    <?php
+                    } else {
+                    ?>
+                      <img src="<?= base_url('icon/wanita.png') ?>" alt="" width="40px">
+                    <?php
+                    }
 
-              <div class="modal-footer">
-                <button type="submit" id="edit" onclick="Swal.fire('SUKSES','Teredit Tersimpan','success')" class="btn btn-primary">EDIT</button>
-                <?php echo form_close(); ?>
+                    ?>
+                  <?php
+                  } else {
+                  ?>
+
+                    <img src="<?= base_url('./assets/guru/' . $gr->ft_guru) ?>" alt="" width="100px" height="100px"><br>
+                  <?php
+                  }
+
+                  ?>
+                  <br>
+                  <div>
+                    <label>*) lewati form ini jika Foto tidak mau di ubah</label>
+                  </div>
+                </div>
+                <br>
+                <div class="modal-footer">
+                  <button type="submit" id="edit" onclick="Swal.fire('SUKSES','Teredit Tersimpan','success')" class="btn btn-primary">EDIT</button>
+                  <?php echo form_close(); ?>
+                </div>
 
                 <script>
                   const tombol = document.querySelector('#edit');
@@ -95,7 +132,6 @@
                   });
                 </script>
               </div>
-
         </div>
       </div>
     </div>
