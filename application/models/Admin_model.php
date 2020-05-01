bffdaea360beeafe8ca70104a55eda7914724af1
MohNuris <nuris.ft.unuja@gmail.com>
2020-05-02 04:48:33 +0700

    joinsiswa admin-model

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -35,6 +35,20 @@ class Admin_model extends CI_Model
   {
     $query = $this->db->select('*')
       ->from('tb_siswa')
+      ->join('tb_kelas', 'tb_siswa.id_kelas=tb_kelas.id_kelas', 'left')
+      ->join('tb_sekolah', 'tb_siswa.id_sekolah=tb_sekolah.id_sekolah', 'right')
+      ->join('tb_jurusan', 'tb_siswa.id_jurusan=tb_jurusan.id_jurusan', 'left')
+      // ->join('tb_guru', 'tb_siswa.id_guru=tb_guru.id_guru', 'left')
+      // ->join('tb_kelas', 'tb_siswa.id_kelas=tb_kelas.id_kelas', 'left')
+      ->order_by('id_siswa', 'DESC')
+      ->get();
+
+    return $query;
+  }
+  public function joinguru()
+  {
+    $query = $this->db->select('*')
+      ->from('tb_siswa')
       ->join('tb_guru', 'tb_siswa.id_guru=tb_guru.id_guru', 'left')
       ->order_by('id_siswa', 'DESC')
       ->get();
