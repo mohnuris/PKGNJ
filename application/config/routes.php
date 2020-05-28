efe15a67253335408ee90e2c2a6e6a7380366604
MohNuris <nuris.ft.unuja@gmail.com>
2020-05-29 01:31:29 +0700

    setleveluser

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['default_controller'] = 'admin';
-$route['users'] = 'admin/users';
+$route['users'] = 'Admin/users';
