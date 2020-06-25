599f308a1827a8a2075c172dd8891757fd70ef0b
MohNuris <nuris.ft.unuja@gmail.com>
2020-06-25 21:33:37 +0700

    AddCombo
    combo, combo1, comb2, combo3, combo4

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -51,30 +51,15 @@ class Admin_model extends CI_Model
       ->from('tb_guru')
       // ->join('tb_siswa', 'tb_guru.id_siswa=tb_guru.id_guru', 'left')
       ->join('tb_kelas', 'tb_guru.id_kelas=tb_kelas.id_kelas', 'left')
-      ->join('tb_sekolah', 'tb_guru.id_sekolah=tb_sekolah.id_sekolah', 'right')
-      ->join('tb_jurusan', 'tb_guru.id_jurusan=tb_jurusan.id_jurusan', 'left')
+      // ->join('tb_sekolah', 'tb_guru.id_sekolah=tb_sekolah.id_sekolah', 'right')
+      // ->join('tb_jurusan', 'tb_guru.id_jurusan=tb_jurusan.id_jurusan', 'left')
       ->order_by('id_guru', 'DESC')
       ->get();
 
     return $query;
   }
 
-  // combokelas
-  public function dinamis1()
-  {
-    $query = $this->db->get('tb_sekolah');
-    $tambah[set_value('id_sekolah')] = "---Pilih---";
-    if ($query->num_rows() > 0) {
-      foreach ($query->result() as $row) {
-        $tambah[$row->id_sekolah] = $row->nm_sekolah;
-      }
-    }
-    return $tambah;
-  }
-
-
-
-  public function comboxdinamis()
+  public function combox()
   {
     $query = $this->db->get('tb_kelas');
     $tambah[set_value('id_kelas')] = "---Pilih---";
@@ -145,64 +130,4 @@ class Admin_model extends CI_Model
     }
     return $tambah;
   }
-
-  // public function comboxdinamis1()
-  // {
-  //   $query = $this->db->get('tb_sekolah');
-  //   $tambah[set_value('nm_sekolah')] = "---Pilih ---";
-  //   if ($query->num_rows() > 0) {
-  //     foreach ($query->result() as $row) {
-  //       $tambah[$row->id_sekolah] = $row->nm_sekolah;
-  //     }
-  //   }
-  //   return $tambah;
-  // }
-
-  // public function comboxdinamis2()
-  // {
-  //   $query = $this->db->get('tb_kelas');
-  //   $tambah[set_value('jurusan')] = "---Pilih ---";
-  //   if ($query->num_rows() > 0) {
-  //     foreach ($query->result() as $row) {
-  //       $tambah[$row->id_kelas] = $row->jurusan;
-  //     }
-  //   }
-  //   return $tambah;
-  // }
-
-  public function comboxdinamis3()
-  {
-    $query = $this->db->get('tb_guru');
-    $tambah[set_value('nm_guru')] = "---Pilih ---";
-    if ($query->num_rows() > 0) {
-      foreach ($query->result() as $row) {
-        $tambah[$row->id_guru] = $row->nm_guru;
-      }
-    }
-    return $tambah;
-  }
-  // public function comboxdinamis4()
-  // {
-  //   $query = $this->db->get('tb_guru');
-  //   $tambah[set_value('mapel')] = "---Pilih ---";
-  //   if ($query->num_rows() > 0) {
-  //     foreach ($query->result() as $row) {
-  //       $tambah[$row->id_guru] = $row->mapel;
-  //     }
-  //   }
-  //   return $tambah;
-  // }
-
-  // comboxdinamis Guru
-  // public function comboxdinamis5()
-  // {
-  //   $query = $this->db->get('tb_kelas');
-  //   $tambah[set_value('id_kelas')] = "---Pilih---";
-  //   if ($query->num_rows() > 0) {
-  //     foreach ($query->result() as $row) {
-  //       $tambah[$row->id_kelas] = $row->id_kelas;
-  //     }
-  //   }
-  //   return $tambah;
-  // }
 }
