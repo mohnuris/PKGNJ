43a114800ae815e362289b9fba6abd96752501e6
mohnuris <nuris.ft.unuja@gmail.com>
2020-07-20 07:54:34 +0700

    halaman soal penilaian
    Signed-off-by: mohnuris <nuris.ft.unuja@gmail.com>

--- application/views/template/soal.php
+++ application/views/template/soal.php
@@ -3,10 +3,10 @@
 
     <main>
         <div class="container-fluid">
-            <h1 class="mt-4">Soal Siswa</h1>
+            <h1 class="mt-4">Halaman Soal</h1>
             <ol class="breadcrumb mb-4">
                 <li class="breadcrumb-item"><a href=" <?= base_url('admin') ?>">Dashboard</a></li>
-                <li class="breadcrumb-item active">Soal Siswa</li>
+                <li class="breadcrumb-item active">Soal</li>
             </ol>
 
             <div class="card  mb-4">
